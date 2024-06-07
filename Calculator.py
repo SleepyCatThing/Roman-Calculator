857b98157acf5332193297046cd440818bf3712f
SleepyCatThing <nickkirby2005@gmail.com>
2024-06-07 13:58:24 -0400

    cleanup of code to look more readable

--- Calculator.py
+++ Calculator.py
@@ -1,40 +1,47 @@
 import tkinter as tk
 window = tk.Tk()
 buttons=[]
-frame_1 = tk.Frame(width=100, height=10)
-frame_3 = tk.Frame(width=100, height=20)
-frame_2 = tk.Frame(width=100, height=100)
-for btn in range (9):
-    if btn in range (3):
-        btnx= tk.Button(
-        text=btn+1,
-        master=frame_1,
-        width=5,
-        height=5
-        )
-    elif btn in range (6):
-        btnx= tk.Button(
-        text=btn+1,
-        master=frame_2,
-        width=5,
-        height=5
-        )
-    elif btn in range (9):
-        btnx= tk.Button(
-        text=btn+1,
-        master=frame_3,
-        width=5,
-        height=5
-        )
-    buttons.append(btnx)
-for btns in range (9):
-    burger = buttons[btns]
-    burger.pack(side=tk.LEFT)
+button_row_1 = tk.Frame(width=100, height=10)
+button_row_3 = tk.Frame(width=100, height=20)
+button_row_2 = tk.Frame(width=100, height=100)
 
 
-frame_3.pack(side=tk.BOTTOM)
-frame_2.pack(side=tk.BOTTOM)
-frame_1.pack(side=tk.BOTTOM)
+for i in range (9):
+
+    if i in range (3):
+        button_adder= tk.Button(
+        text=i+1,
+        master=button_row_1,
+        width=5,
+        height=5
+        )
+
+    elif i in range (6):
+        button_adder= tk.Button(
+        text=i+1,
+        master=button_row_2,
+        width=5,
+        height=5
+        )
+
+    elif i in range (9):
+        button_adder= tk.Button(
+        text=i+1,
+        master=button_row_3,
+        width=5,
+        height=5
+        )
+
+    buttons.append(button_adder)
+    
+for j in range (9):
+    button_packer = buttons[j]
+    button_packer.pack(side=tk.LEFT)
+
+
+button_row_3.pack(side=tk.BOTTOM)
+button_row_2.pack(side=tk.BOTTOM)
+button_row_1.pack(side=tk.BOTTOM)
 
 
 

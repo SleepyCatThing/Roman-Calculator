a863a01285632bbb1bc9796d32ad441918b574c6
SleepyCatThing <nickkirby2005@gmail.com>
2024-06-07 14:10:42 -0400

    tiny bit of cleanup done

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,13 @@
 import tkinter as tk
+
+# Initializing my silly stuff that doesn't need silly operations done to add
 window = tk.Tk()
 buttons=[]
 button_row_1 = tk.Frame(width=100, height=10)
 button_row_3 = tk.Frame(width=100, height=20)
 button_row_2 = tk.Frame(width=100, height=100)
 
-
+# Big, messy and unoptimal loop to make a 9 button display. Each inner loop adds a row. 
 for i in range (9):
 
     if i in range (3):
@@ -33,12 +35,11 @@ for i in range (9):
         )
 
     buttons.append(button_adder)
-    
+
 for j in range (9):
-    button_packer = buttons[j]
-    button_packer.pack(side=tk.LEFT)
+    buttons[j].pack(side=tk.LEFT)
 
-
+# this doesn't need a comment but i think itd be funny
 button_row_3.pack(side=tk.BOTTOM)
 button_row_2.pack(side=tk.BOTTOM)
 button_row_1.pack(side=tk.BOTTOM)

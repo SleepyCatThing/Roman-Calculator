312b86ee6109c748cdfd46f8e1c90005d2158768
SleepyCatThing <nickkirby2005@gmail.com>
2024-06-07 14:42:28 -0400

    Beginning work on display w usable buttons

--- Calculator.py
+++ Calculator.py
@@ -7,41 +7,22 @@ button_row_1 = tk.Frame(width=100, height=10)
 button_row_3 = tk.Frame(width=100, height=20)
 button_row_2 = tk.Frame(width=100, height=100)
 
-# Big, messy and unoptimal loop to make a 9 button display. Each inner loop adds a row. 
-for i in range (9):
+def add(x, y):
+    return x + y
+def sub(x, y):
+    return x - y
+def div(x, y):
+    return x / y
+def mult(x, y):
+    return x * y
 
-    if i in range (3):
-        button_adder= tk.Button(
-        text=i+1,
-        master=button_row_1,
-        width=5,
-        height=5
-        )
-
-    elif i in range (6):
-        button_adder= tk.Button(
-        text=i+1,
-        master=button_row_2,
-        width=5,
-        height=5
-        )
-
-    elif i in range (9):
-        button_adder= tk.Button(
-        text=i+1,
-        master=button_row_3,
-        width=5,
-        height=5
-        )
-
-    buttons.append(button_adder)
-
-for j in range (9):
-    buttons[j].pack(side=tk.LEFT)
-
-# this doesn't need a comment but i think itd be funny
-button_row_3.pack(side=tk.BOTTOM)
-button_row_2.pack(side=tk.BOTTOM)
-button_row_1.pack(side=tk.BOTTOM)
+def button_press(button):
+    return(button.int())
 
 window.mainloop()
+# TO DO:
+# Make buttons again
+# execute button_press on tk.Button() click or smth 
+# Make working top display
+# Convert top display to roman numerals
+# figure out what the fuck im doing
